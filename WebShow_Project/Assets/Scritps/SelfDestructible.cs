335b2d78a8a34417603f7c3076272940424ffdf2
theixgamer <theofritaku@gmail.com>
2020-09-03 20:27:53 -0300

    puntos autodestruibles

---
+++ WebShow_Project/Assets/Scritps/SelfDestructible.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestructible : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public enum TypeDestruction
+    {
+        SetActive,
+        Destroy,
+        None,
+    }
+    public TypeDestruction typeDestruction = TypeDestruction.SetActive;
+    public float delayDestroy;
+    private float auxDelayDestroy;
+    void Start()
+    {
+        auxDelayDestroy = delayDestroy;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (typeDestruction != TypeDestruction.None)
+        {
+            CheckDelayDestroy();
+        }
+    }
+    public void CheckDelayDestroy()
+    {
+        if (delayDestroy > 0)
+        {
+            delayDestroy = delayDestroy - Time.deltaTime;
+        }
+        else
+        {
+            delayDestroy = auxDelayDestroy;
+            switch (typeDestruction)
+            {
+                case TypeDestruction.Destroy:
+                    Destroy(gameObject);
+                    break;
+                case TypeDestruction.SetActive:
+                    gameObject.SetActive(false);
+                    break;
+            }
+        }
+    }
+}
